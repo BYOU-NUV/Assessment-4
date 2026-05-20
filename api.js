35b6d21ef79f0accff355e731fd43ee4288fc4d0
BYOU-NUV <benoit.you@univ.edu.vu>
2026-05-20 12:47:26 +1100

    Res.sen

--- api.js
+++ api.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
   res.send('It is the stagging development:all good!');
 });
 
+app.get('/api', (req, res) => {​
+  res.sen('It is another API');​
+
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
