d1d5bfef7eae86e2b9ac1b053d633c55b1551da5
BYOU-NUV <benoit.you@univ.edu.vu>
2026-05-20 15:36:48 +1100

    cREATION OF A NEW API

--- api.js
+++ api.js
@@ -1,19 +1,17 @@
 // index.js
 const express = require('express');
 const app = express();
-
 const visiteur = process.env.visiteur || 'Anonyme';
 
 app.get('/', (req, res) => {
-  res.send('It is the staging development: all good!');
+  res.send('It is the stagging development:all good!');
 });
 
 app.get('/api', (req, res) => {
-  res.sen('It is another API');
+  res.sen('New API');
 });
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
