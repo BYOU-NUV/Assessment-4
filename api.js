9ea2b99ea4ee0cefbb24b97576006ed4855d01f2
BYOU-NUV <benoit.you@univ.edu.vu>
2026-05-20 12:39:34 +1100

    New api with error

--- api.js
+++ api.js
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
   res.send('It is the stagging development:all good!');
 });
 
+app.get('/api', (req, res) => {​
+
+  re.send('It is another API');​
+
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
